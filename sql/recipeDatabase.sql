e941b83d22906d82928d693d07407fcfb22bd41e
cynthiacodes <cjnkstudy01@hotmail.com>
2023-09-12 21:27:46 +0100

    created recipes table and insert data

--- sql/recipeDatabase.sql
+++ sql/recipeDatabase.sql
@@ -1,5 +1,25 @@
-drop table if exists recipes
+DROP TABLE IF EXISTS RECIPES
 
-create table recipes(
+CREATE TYPE MEAL_TIME AS ENUM ('Breakfast', 'Lunch', 'Dinner', 'Snack');
 
-);
+
+CREATE TABLE RECIPES(
+    id serial primary key,
+    title varchar(255),
+    meal_type MEAL_TIME,
+    cuisine varchar(100),
+    video_url varchar(255) 
+    );
+
+INSERT INTO RECIPES (title, meal_type, cuisine, video_url)
+VALUES
+    ('Sukuma Wiki with Ugali', 'Dinner', 'Kenyan', 'https://www.youtube.com/watch?v=7seIPgxQOY0&ab_channel=InfoodsSpecials'),
+    
+    ('Mahamri', 'Breakfast', 'Kenyan', 'https://www.youtube.com/watch?v=IwKwztK8Yow&ab_channel=RukiaLaltia'),
+
+    ('Matoke', 'Dinner', 'Kenyan', 'https://www.youtube.com/watch?v=RWJ4uRVLlE0&ab_channel=CookingTastyAfricanDishes'),
+    
+    ('samosa', 'Breakfast', 'Kenyan', 'https://www.youtube.com/watch?v=S-j-LxKB-Eg&ab_channel=AtasteofhomewithViv');
+
+
+
