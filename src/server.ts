00ee57027dec2ce8480ebc036539f4311c42664b
cynthiacodes <cjnkstudy01@hotmail.com>
2023-09-20 18:45:04 +0100

    added post and delete recipe route

--- src/server.ts
+++ src/server.ts
@@ -17,6 +17,13 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
 
+interface Recipe {
+    title: string;
+    meal_type: string;
+    cuisine: string;
+    video_url: string;
+}
+
 app.get("/", async (_req, res) => {
     res.json({
         msg: "Hello! There's nothing interesting for GET / from this recipe backend server",
@@ -27,6 +34,36 @@ app.get("/recipes", async (_req, res) => {
     try {
         const response = await client.query("select * from recipes");
         res.status(200).json(response.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("An error occurred. Check server logs.");
+    }
+});
+
+app.post<{}, {}, Recipe>("/recipes", async (req, res) => {
+    try {
+        const { title, meal_type, cuisine, video_url } = req.body;
+        const insertQuery =
+            "INSERT INTO RECIPES (title, meal_type, cuisine, video_url) VALUES ($1,$2,$3,$4) RETURNING * ";
+        const values = [title, meal_type, cuisine, video_url];
+        const response = await client.query(insertQuery, values);
+
+        res.status(201).json(response.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("An error occurred. Check server logs.");
+    }
+});
+
+app.delete<{ id: number }>("/recipes/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const deleteQuery = "DELETE FROM RECIPES WHERE  id = $1 RETURNING *";
+        const value = [id];
+        const response = await client.query(deleteQuery, value);
+        res.status(200).json({
+            deletedRecipe: response.rows[0],
+        });
     } catch (error) {
         console.error(error);
         res.status(500).send("An error occurred. Check server logs.");
